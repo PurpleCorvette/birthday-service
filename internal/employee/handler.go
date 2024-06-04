c113e40507ac6c397cbd686d1154150537b266c8
PurpleCorvette <lesoboleff@mail.ru>
2024-06-04 23:07:59 +0300

    add api logic

--- internal/employee/handler.go
+++ internal/employee/handler.go
@@ -19,74 +19,89 @@ func NewEmployeeHandler(service EmployeeService) *EmployeeHandler {
 func (h *EmployeeHandler) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	switch r.Method {
 	case "POST":
-		var emp Employee
-		if err := json.NewDecoder(r.Body).Decode(&emp); err != nil {
-			http.Error(w, "invalid request payload", http.StatusBadRequest)
-			return
-		}
-
-		newEmp, err := h.service.AddEmployee(emp.Name, emp.DOB)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusCreated)
-		json.NewEncoder(w).Encode(newEmp)
-
+		h.CreateEmployee(w, r)
 	case "GET":
-		params := mux.Vars(r)
-		id, err := strconv.Atoi(params["id"])
-		if err != nil {
-			http.Error(w, "invalid employee ID", http.StatusBadRequest)
-			return
-		}
-
-		emp, err := h.service.GetEmployee(id)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusNotFound)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(emp)
-
+		h.GetEmployee(w, r)
 	case "PUT":
-		params := mux.Vars(r)
-		id, err := strconv.Atoi(params["id"])
-		if err != nil {
-			http.Error(w, "invalid employee ID", http.StatusBadRequest)
-			return
-		}
-
-		var emp Employee
-		if err := json.NewDecoder(r.Body).Decode(&emp); err != nil {
-			http.Error(w, "invalid request payload", http.StatusBadRequest)
-			return
-		}
-
-		updateEmp, err := h.service.UpdateEmployee(id, emp.Name, emp.DOB)
-		if err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-			return
-		}
-
-		w.WriteHeader(http.StatusOK)
-		json.NewEncoder(w).Encode(updateEmp)
+		h.UpdateEmployee(w, r)
 	case "DELETE":
-		params := mux.Vars(r)
-		id, err := strconv.Atoi(params["id"])
-		if err != nil {
-			http.Error(w, "invalid employee id", http.StatusBadRequest)
-			return
-		}
-
-		if err := h.service.DeleteEmployee(id); err != nil {
-			http.Error(w, err.Error(), http.StatusInternalServerError)
-		}
-
-		w.WriteHeader(http.StatusOK)
+		h.DeleteEmployee(w, r)
 	default:
 		http.Error(w, "Method not allowed", http.StatusMethodNotAllowed)
 	}
+}
+
+func (h *EmployeeHandler) CreateEmployee(w http.ResponseWriter, r *http.Request) {
+	var emp Employee
+	if err := json.NewDecoder(r.Body).Decode(&emp); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	newEmp, err := h.service.AddEmployee(emp.Name, emp.DOB)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newEmp)
+}
+
+func (h *EmployeeHandler) GetEmployee(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid employee ID", http.StatusBadRequest)
+		return
+	}
+
+	emp, err := h.service.GetEmployee(id)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusNotFound)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(emp)
+}
+
+func (h *EmployeeHandler) UpdateEmployee(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid employee ID", http.StatusBadRequest)
+		return
+	}
+
+	var emp Employee
+	if err := json.NewDecoder(r.Body).Decode(&emp); err != nil {
+		http.Error(w, "Invalid request payload", http.StatusBadRequest)
+		return
+	}
+
+	updatedEmp, err := h.service.UpdateEmployee(id, emp.Name, emp.DOB)
+	if err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusOK)
+	json.NewEncoder(w).Encode(updatedEmp)
+}
+
+func (h *EmployeeHandler) DeleteEmployee(w http.ResponseWriter, r *http.Request) {
+	params := mux.Vars(r)
+	id, err := strconv.Atoi(params["id"])
+	if err != nil {
+		http.Error(w, "Invalid employee ID", http.StatusBadRequest)
+		return
+	}
+
+	if err := h.service.DeleteEmployee(id); err != nil {
+		http.Error(w, err.Error(), http.StatusInternalServerError)
+		return
+	}
+
+	w.WriteHeader(http.StatusNoContent)
 }
